f443671c00818fcd387353fe2fff590a5a81fb90
Bonnie Reed <bonniejeanreed@gmail.com>
2021-10-25 10:33:57 -0500

    changed GET and POST routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const { clog } = require('./middleware/clog');
+const notes = require('./db/notes.json');
 const api = require('./routes/indexRoute');
 
 const PORT = process.env.port || 3001;
@@ -17,12 +17,12 @@ const middleware = (req, res, next) => {
     next();
 };
 app.use(middleware);
-
-app.get('/', (req, res) => res.json(`GET route`));
-app.post('/', (req, res) => res.json(`POST route`));
-app.put('/:id', (req, res) => res.json(`PUT route`));
-app.delete('/:id', (req, res) => res.json(`DELETE route`));
-app.patch('/:id', (req, res) => res.json(`PATCH route`));
+//api routes send notes in the 'get route
+app.get('/api/notes', (req, res) => res.json(notes));
+app.post('/api/notes', (req, res) => res.json(`POST route`));
+app.put('/api/notes/:id', (req, res) => res.json(`PUT route`));
+app.delete('/api/notes/:id', (req, res) => res.json(`DELETE route`));
+app.patch('/api/notes/:id', (req, res) => res.json(`PATCH route`));
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
@@ -33,12 +33,12 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 // GET Route for notes page
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 app.listen(PORT, () =>
